85faa3b29a8790a72a39fe4e05c43d2900b00b5c
immxalan <aedocoding@gmail.com>
2020-10-16 16:14:43 -0700

    Edits to auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -50,10 +50,10 @@ router.post("/login", (req, res) => {
     .then((user) => {
       if (isValid(req.body)) {
         if (user && bcrypt.compareSync(password, user.password)) {
-          const token = generateToken(user); //get a token
+          const token = generateToken(user); 
           res.status(200).json({
             message: `Welcome ${user.username}!`,
-            token, //send the token
+            token, 
           });
         } else {
           res.status(401).json({ message: "Invalid Credentials" });
@@ -63,7 +63,7 @@ router.post("/login", (req, res) => {
       }
     })
     .catch((error) => {
-      res.status(500).json(error);
+      res.status(500).json(error.message);
     });
 });
 function generateToken(user) {
