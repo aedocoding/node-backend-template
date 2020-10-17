5f56a1e8930bba7f119e051d010cc6b1bda76b79
immxalan <aedocoding@gmail.com>
2020-10-16 18:53:26 -0700

    Fixed typo in knex file

--- knexfile.js
+++ knexfile.js
@@ -41,10 +41,10 @@ module.exports = {
       max: 10,
     },
     migrations: {
-      directory: "./database/migrations",
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: "./database/seeds",
+      directory: "./data/seeds",
     },
     useNullAsDefault: true,
   },
