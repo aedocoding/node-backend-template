08188ac47cc1fa8385217622dc95c79491ed4596
immxalan <aedocoding@gmail.com>
2020-10-16 17:29:47 -0700

    Updated readme, added db endpoint

--- api/server.js
+++ api/server.js
@@ -8,7 +8,28 @@ server.use(helmet());
 server.use(CORS());
 server.use(express.json());
 server.use("/auth", authRouter);
-server.get("/", (req, res) => {
-  res.status(200).json({ message: "Server is running" });
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+server
+  .get("/", (req, res) => {
+    res.status(200).json({ message: "Server is running" });
+  })
+  .get("/db", async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM test_table");
+      const results = { results: result ? result.rows : null };
+      res.render("pages/db", results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  });
 module.exports = server;
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const server = require("./api/server.js");
-const port = process.env.PORT;
-server.listen(port, () => 
-  console.log(`Server is running on ${port}`)
-);
 
+const port = process.env.PORT;
+
+server.listen(port, () => console.log(`Server is running on ${port}`));
