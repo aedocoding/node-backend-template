53e2a6a6b00560d24ab35c45b668f1e5aad8d2ea
immxalan <aedocoding@gmail.com>
2020-10-19 14:50:57 -0700

    Trying promise version of router again

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,90 +5,80 @@ const { jwtSecret } = require("../config/secret");
 const Users = require("../users/user-model");
 const { isValid } = require("../users/user-service");
 
-router.post("/register", async (req, res, next) => {
+router.post("/register", (req, res) => {
   const user = req.body;
-  const userExist = await Users.findBy({ username: user.username }).first();
-  if (userExist) {
-    res.status(400).json({
-      message: `Username of ${user.username} already exists, please register with a different username or login`,
-    });
-    return;
-  }
-  const emailExist = await Users.findBy({ email: user.email }).first();
-  if (emailExist) {
-    res.status(400).json({
-      message: `Email of ${user.email} already exists, please register with a different email or login`,
-    });
-    return;
-  }
+  // const userExist = await Users.findBy({ username: user.username }).first();
+  // if (userExist) {
+  //   res.status(400).json({
+  //     message: `Username of ${user.username} already exists, please register with a different username or login`,
+  //   });
+  //   return;
+  // }
+  // const emailExist = await Users.findBy({ email: user.email }).first();
+  // if (emailExist) {
+  //   res.status(400).json({
+  //     message: `Email of ${user.email} already exists, please register with a different email or login`,
+  //   });
+  //   return;
+  // }
   const rounds = process.env.BCRYPT_ROUNDS
     ? parseInt(process.env.BCRYPT_ROUNDS)
     : 10;
   const hash = bcrypt.hashSync(user.password, rounds);
   user.password = hash;
-  try {
-    if (isValid(user)) {
-      const newUser = await Users.add(user);
-      res.status(201).json({
-        auth: {
-          id: newUser.id,
-          name: newUser.name,
-          email: newUser.email,
-        },
-      });
-    } else {
-      next({
-        apiCode: 400,
-        apiMessage: 'name, email or password missing',
-      });
-    }
-  } catch (error) {
-    next({ apiCode: 500, apiMessage: 'error saving new user', ...error });
-  }
-
-  // if (isValid(newUser)) {
-  //   Users.add(newUser)
-  //     .then((saved) => {
-  //       res.status(201).json(saved);
-  //     })
-  //     .catch((error) => {
-  //       res.status(500).json(error.message);
-  //     });
+  // try {
+  //   if (isValid(user)) {
+  //     await Users.add(user);
+  //     res.status(201).json(user);
+  //   } else {
+  //     res.status(400).json({message: 'Username, password, or email is missing'});
+  //   }
+  // } catch (error) {
+  //  res.status(500).json(error.message)
   // }
-  // Users.findBy({ username: newUser.username })
-  //   .first()
-  //   .then((e) => {
-  //     if (e) {
-  //       res.status(400).json({
-  //         message: `Username of ${e.username} already exists, please register with a different username or login`,
-  //       });
-  //     } else {
-  //       Users.findBy({ email: newUser.email })
+  if (isValid(newUser)) {
+    Users.add(newUser)
+      .then((saved) => {
+        res.status(201).json(saved);
+      })
+      .catch((error) => {
+        res.status(500).json(error.message);
+      });
+  }
+  Users.findBy({ username: newUser.username })
+    .first()
+    .then((e) => {
+      if (e) {
+        res.status(400).json({
+          message: `Username of ${e.username} already exists, please register with a different username or login`,
+        });
+      } else {
+        Users.findBy({ email: newUser.email })
 
-  //         .first()
-  //         .then(e => {
-  //           if (e) {
-  //             res.status(400).json({
-  //               message: `Email of ${e.email} already exists, please register with a different email or login`,
-  //             });
-  //           } else {
-  //             if (isValid(newUser)) {
-  //               Users.add(newUser)
-  //                 .then(saved => {
-  //                   res.status(201).json(saved);
-  //                 })
-  //                 .catch(error => {
-  //                   res.status(500).json(error.message);
-  //                 });
-  //             } else {
-  //               res
-  //                 .status(400)
-  //                 .json({ message: "Username, email, or password missing" });
-  //             }
-  //           }
-  //         });
-  //     }
-  //   });
+          .first()
+          .then((e) => {
+            if (e) {
+              res.status(400).json({
+                message: `Email of ${e.email} already exists, please register with a different email or login`,
+              });
+            } else {
+              if (isValid(newUser)) {
+                Users.add(newUser)
+                  .then((saved) => {
+                    res.status(201).json(saved);
+                  })
+                  .catch((error) => {
+                    res.status(500).json(error.message);
+                  });
+              } else {
+                res
+                  .status(400)
+                  .json({ message: "Username, email, or password missing" });
+              }
+            }
+          });
+      }
+    });
 });
 
 router.post("/login", (req, res) => {
